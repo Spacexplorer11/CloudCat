05f442bb6ecb02b36a99bc993dbc55f7215652fd
Akaalroop Singh <spacexplorer11@proton.me>
2025-08-11 11:41:41 +0100

    Added cloud movement! Now it moves left continuously!
    Signed-off-by: Akaalroop Singh <spacexplorer11@proton.me>

--- src/main.rs
+++ src/main.rs
@@ -2,28 +2,33 @@ use macroquad::prelude::*;
 
 #[macroquad::main("CloudCat")]
 async fn main() {
+    // Catty variables :3
     let mut cat_frame = 0;
     let mut cat_timer = 0.0;
+    let cat_run_speed = 0.1;
+
+    // Cloud variables ☁
     let mut cloud_frame = 0;
     let mut cloud_timer = 0.0;
+    let mut cloud_x = 350.0;
     loop {
         clear_background(WHITE);
 
         draw_text("CloudCat", screen_width() * 0.45, 50.0, 50.0, DARKGRAY);
 
-        (cloud_timer, cloud_frame) = draw_cloud(cloud_timer, cloud_frame).await;
+        (cloud_timer, cloud_frame) = draw_cloud(cloud_timer, cloud_frame, cloud_x).await;
 
-        (cat_timer, cat_frame) = draw_cat(cat_timer, cat_frame).await;
+        (cat_timer, cat_frame) = draw_cat(cat_timer, cat_frame, cat_run_speed).await;
+
+        cloud_x -= 1.0;
 
         next_frame().await
     }
 }
 
-async fn draw_cat(mut timer: f32, mut frame: i32) -> (f32, i32) {
+async fn draw_cat(mut timer: f32, mut frame: i32, cat_run_speed: f32) -> (f32, i32) {
     let cat: Texture2D = load_texture("assets/cat.png").await.unwrap();
     cat.set_filter(FilterMode::Nearest);
-
-    let cat_run_speed = 0.1;
 
     let frame_width = 32.0;
     let frame_height = 32.0;
@@ -52,7 +57,7 @@ async fn draw_cat(mut timer: f32, mut frame: i32) -> (f32, i32) {
     (timer, frame)
 }
 
-async fn draw_cloud(mut timer: f32, mut frame: i32) -> (f32, i32) {
+async fn draw_cloud(mut timer: f32, mut frame: i32, cloud_x: f32) -> (f32, i32) {
     let cloud: Texture2D = load_texture("assets/cloud.png").await.unwrap();
     cloud.set_filter(FilterMode::Nearest);
 
@@ -62,7 +67,7 @@ async fn draw_cloud(mut timer: f32, mut frame: i32) -> (f32, i32) {
     let frame_height = 32.0;
     draw_texture_ex(
         &cloud,
-        350.0,
+        cloud_x,
         screen_height() - 390.0,
         WHITE,
         DrawTextureParams {
